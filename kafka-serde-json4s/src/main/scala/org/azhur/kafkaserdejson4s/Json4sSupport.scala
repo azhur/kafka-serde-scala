a6e91e5275747b0779780b9847fef2ee532d5b55
Artur Zhurat <artur.zhurat@gmail.com>
2018-06-02 23:14:00 +0300

    handle errors

--- kafka-serde-json4s/src/main/scala/org/azhur/kafkaserdejson4s/Json4sSupport.scala
+++ kafka-serde-json4s/src/main/scala/org/azhur/kafkaserdejson4s/Json4sSupport.scala
@@ -19,27 +19,38 @@ package org.azhur.kafkaserdejson4s
 import java.nio.charset.StandardCharsets
 import java.util
 
+import org.apache.kafka.common.errors.SerializationException
 import org.apache.kafka.common.serialization.{ Deserializer, Serde, Serializer }
 import org.json4s.{ Formats, Serialization }
 
 import scala.language.implicitConversions
+import scala.util.control.NonFatal
+import scala.util.{ Failure, Success, Try }
 
 trait Json4sSupport {
   implicit def toSerializer[T <: AnyRef](implicit serialization: Serialization,
                                          formats: Formats): Serializer[T] = new Serializer[T] {
     override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
+    override def close(): Unit                                                 = {}
     override def serialize(topic: String, data: T): Array[Byte] =
-      serialization.write[T](data).getBytes(StandardCharsets.UTF_8)
-    override def close(): Unit = {}
+      Try(serialization.write[T](data).getBytes(StandardCharsets.UTF_8)) match {
+        case Success(result)      => result
+        case Failure(NonFatal(e)) => throw new SerializationException(e)
+        case Failure(e)           => throw e
+      }
   }
 
   implicit def toDeserializer[T <: AnyRef: Manifest](implicit serialization: Serialization,
                                                      formats: Formats): Deserializer[T] =
     new Deserializer[T] {
       override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
+      override def close(): Unit                                                 = {}
       override def deserialize(topic: String, data: Array[Byte]): T =
-        serialization.read[T](new String(data, StandardCharsets.UTF_8))
-      override def close(): Unit = {}
+        Try(serialization.read[T](new String(data, StandardCharsets.UTF_8))) match {
+          case Success(result)      => result
+          case Failure(NonFatal(e)) => throw new SerializationException(e)
+          case Failure(e)           => throw e
+        }
     }
 
   implicit def toSerde[T <: AnyRef: Manifest](implicit serialization: Serialization,
